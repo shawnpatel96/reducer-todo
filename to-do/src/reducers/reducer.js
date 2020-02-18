51c88d0b734e2c62596647df1e8f7c03a33537f4
shawn <shawnpatel196@gmail.com>
2020-02-17 22:09:30 -0600

    fixed blank LI issue, FINALLY

--- to-do/src/reducers/reducer.js
+++ to-do/src/reducers/reducer.js
@@ -5,6 +5,16 @@ export const initialState =[
         task: 'Learn about reducers',
         completed: false,
         id: 3892987589
+    },
+    {
+        task: 'Learn about reducers part 2',
+        completed: false,
+        id: 3892987584
+    },
+    {
+        task: 'Learn about reducers part 3?',
+        completed: false,
+        id: 3892987584
     }
 
 ]
@@ -13,8 +23,13 @@ export const reducer = (state,action) => {
     console.log("inside the reducer", action)
     switch(action.type) {
         case "ADD_TASK":
-            const newTask={...action.payload, id: Date.now(), completed:false}
-            return [...state, newTask]
+            return [
+                ...state, {
+                    task:action.payload,
+                    completed: false,
+                    id: Date.now()
+                }
+            ]
         case "TOGGLE_TASK":
             return state.map(item=>{
                 return item.id === action.payload ? {...item, completed: !item.completed}:item});
